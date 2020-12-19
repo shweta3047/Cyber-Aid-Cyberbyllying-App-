5c9f05283d60f98fb626a5e0f1fd851cf30c40aa
shweta3047 <shweta3047@gmail.com>
2020-12-19 13:27:55 +0530

    added theme

---
+++ constants/theme.js
@@ -0,0 +1,69 @@
+const colors = {
+  accent: '#e6e1e4',
+  primary: '#2c2b3f',
+  secondary: '#e6e1e4',
+  tertiary: '#4fdbdb',
+  purple:'#7a69b0',
+  creamWhite:'#e6e1e4',
+  brownishgrey: '#645c64',
+  black: '#000000',
+  white: '#FFFFFF',
+  white2: 'rgba(255,255,255,0.5)',
+  gray: '#9DA3B4',
+  gray2: '#C5CCD6',
+  gray3: 'rgba(197, 204, 214, 0.4)',
+  blueGrey: '#5b6477',
+  lightBlueGrey: '#95a6bc',
+  red: '#FF0000',
+  lightGrey: '#f5f5f5',
+  lightPink: '#ffecf2',
+  redPink: '#f7536e',
+  pastelPink: '#ffd1dc',
+  lightLilac: '#ffefef',
+  cream: '#fdfff5',
+  brown: '#8d5845',
+  yellow: '#face1b'
+};
+
+const sizes = {
+  // global sizes
+  base: 16,
+  font: 14,
+  radius: 6,
+  padding: 25,
+
+  // font sizes
+  h1: 26,
+  h2: 20,
+  h3: 18,
+  title: 18,
+  header: 16,
+  body: 14,
+  caption: 12
+};
+
+const fonts = {
+  h1: {
+    fontSize: sizes.h1
+  },
+  h2: {
+    fontSize: sizes.h2
+  },
+  h3: {
+    fontSize: sizes.h3
+  },
+  header: {
+    fontSize: sizes.header
+  },
+  title: {
+    fontSize: sizes.title
+  },
+  body: {
+    fontSize: sizes.body
+  },
+  caption: {
+    fontSize: sizes.caption
+  }
+};
+
+export { colors, sizes, fonts };
