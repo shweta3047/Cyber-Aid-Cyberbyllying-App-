5f5c2b4ba7a3a2a0526ba3b2ca67c08eb6bc244e
shweta3047 <shweta3047@gmail.com>
2020-12-19 13:24:11 +0530

    added screens setup

---
+++ screens/parent-screens/ProfileScreen.js
@@ -0,0 +1,21 @@
+import React,{useState} from 'react';
+
+import { View, Text, StyleSheet ,Image,ScrollView,Dimensions, FlatList} from 'react-native';
+import {colors,fonts} from '../../constants/theme';
+
+
+const Profile = (props) => {
+    
+    return (
+        <View style={{flex:1,backgroundColor:colors.primary,display:'flex',width:width}}>
+            
+        </View>
+    )
+
+}
+
+export default Profile
+
+const styles = StyleSheet.create({
+ 
+  });
